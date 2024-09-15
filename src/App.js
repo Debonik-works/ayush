619ddcbf416201c03e099d2bb14e63361a0ef4c0
DebonikINNN <debonikghosh@gmail.com>
2024-09-15 12:00:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import "./App.css";
 import LoginSignup from "./components/LoginSignup/LoginSignup";
 import RegistrationForm from "./components/Registration/RegistrationForm";
 
-const [page, setPage] = useState(0);
+
 function App() {
   return (
     <div>
-      {page == 0 && <LoginSignup />}
+      <LoginSignup />
 
-      {page == 1 && <RegistrationForm />}
+     <RegistrationForm />
     </div>
   );
 }
